3441fd35688b1676c14d21b16aaa2bafca96c402
gianlucadebache <gianluca.debache@edu.htl-klu.at>
2023-11-30 08:40:38 +0100

    Initial Commit

---
+++ Moonbeamer/MoonbeamerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonbeamerApp.swift
+//  Moonbeamer
+//
+//  Created by Gianluca de Bache on 30.11.23.
+//
+
+import SwiftUI
+
+@main
+struct MoonbeamerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
